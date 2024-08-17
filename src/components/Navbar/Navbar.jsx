2e5a3a35152afc5d4fe32f480c027af57a1bae6c
Naseem Ansari <mrnaseem745@gmail.com>
2024-08-18 00:16:55 +0530

    #checking card props

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import Button from '../Button/Button'
 import styles from './Navbar.module.css'
+import Card from '../Card/Card'
+
+const foodCard = [
+    {
+        name: 'Spaghetti Carbonara',
+        img: 'src\assets\food_1.png',
+        des: "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper. It's creamy, savory, and perfectly seasoned.",
+        price: `$20.00`,
+    },
+    {
+        name: 'Spaghetti Carbonara',
+        img: 'src\assets\food_1.png',
+        des: "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper. It's creamy, savory, and perfectly seasoned.",
+        price: `$20.00`,
+    },
+    {
+        name: 'Spaghetti Carbonara',
+        img: 'src\assets\food_1.png',
+        des: "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper. It's creamy, savory, and perfectly seasoned.",
+        price: `$20.00`,
+    },
+]
 
 const Navbar = () => {
   return (
@@ -14,6 +36,11 @@ const Navbar = () => {
             <Button btnText="Breakfast" />
             <Button btnText="Lunch" />
             <Button btnText="Dinner" />
+        </div>
+        <div className="foodcar">
+        {foodCard.map((food) => {
+        <Card cardImg={food.img} cardTitle={food.name} cardText={food.des} cardPrice={food.price}/>
+    })}
         </div>
     </section>
   )
