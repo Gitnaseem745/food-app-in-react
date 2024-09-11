2abc3d869df884a258b4bb500cd41ec403b67321
Naseem Ansari <mrnaseem745@gmail.com>
2024-09-11 23:05:23 +0530

    #work started again

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,7 @@ function App() {
   return (
     <>
     <Navbar />
-    {/* {foodCard.map((food) => {
-        <Card cardImg={food.img} cardTitle={food.name} cardText={food.des} cardPrice={food.price}/>
-    })} */}
     </>
   )
 }
-
-// const foodCard = [
-//     {
-//         'name': 'Spaghetti Carbonara',
-//         'img': './assets/food_1.png',
-//         'des': "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper. It's creamy, savory, and perfectly seasoned.",
-//         'price': `$20.00`,
-//     }
-// ]
-
 export default App
